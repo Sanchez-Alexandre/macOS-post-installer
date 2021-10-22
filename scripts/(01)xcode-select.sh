7d5f76d241be8d883b1fde81cfe057e10c6cc9ec
Dorian Eydoux <loaves_pickle0@icloud.com>
2021-10-22 16:32:59 +0200

    Fix type

--- scripts/(01)xcode-select.sh
+++ scripts/(01)xcode-select.sh
@@ -2,5 +2,5 @@ if ! xcode-select -p 1>/dev/null
 then
   echo "Installing macOS command line tools..."
   xcode-select --install
-  read -p "Press enter when install done..."
+  read -p "Press RETURN to continue..."
 fi
